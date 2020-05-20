7eb8b9fe73323576fa560ecdf32ddc79ce0cca64
thpo <thpoirier@gmail.com>
2020-05-20 02:34:08 +0200

    build version

---
+++ build/precache-manifest.5184e3ec4b15e859f81a9517406e31f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc47435c833ce86874256c3e98739ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9105fac4084104c2ae2",
+    "url": "/static/css/main.170f56e8.chunk.css"
+  },
+  {
+    "revision": "1a8542767918ce89636f",
+    "url": "/static/js/2.19881f45.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.19881f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9105fac4084104c2ae2",
+    "url": "/static/js/main.ad924bad.chunk.js"
+  },
+  {
+    "revision": "58cb158aa2d546ca4898",
+    "url": "/static/js/runtime-main.33f659d6.js"
+  }
+]);
